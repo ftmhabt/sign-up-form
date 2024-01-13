86a4c870d13e2c7f5efcf2d1680a0ad12b1402ee
ftmhabt <ftmh.a78@gmail.com>
2024-01-13 16:08:42 +0330

    Add matching password check

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("set to go");
+let pass=document.querySelector('#password');
+let conPass=document.querySelector('#confirm-password');
+let passSpan=document.querySelector('.pass-match');
+let btn=document.querySelector('button');
+
+conPass.addEventListener('keyup', ()=>{
+    pass.value === conPass.value ? passSpan.textContent='' : passSpan.textContent='password doesn\'t match';
+})
+
+console.log(pass);
+console.log(conPass);
+console.log(passSpan);
+// btn.addEventListener('invalid',()=>{
+
+// })
